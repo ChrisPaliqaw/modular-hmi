157d627fb726ad9343af259112f8445a692234de
Chris Paliqaw <chris@qalang.com>
2023-03-29 12:30:30 +0200

    feature: admin page todo: simplify and fix edit form and logic

---
+++ api/src/services/users/users.scenarios.ts
@@ -0,0 +1,23 @@
+import type { Prisma, User } from '@prisma/client'
+import type { ScenarioData } from '@redwoodjs/testing/api'
+
+export const standard = defineScenario<Prisma.UserCreateArgs>({
+  user: {
+    one: {
+      data: {
+        email: 'String6056392',
+        hashedPassword: 'String',
+        salt: 'String',
+      },
+    },
+    two: {
+      data: {
+        email: 'String5360588',
+        hashedPassword: 'String',
+        salt: 'String',
+      },
+    },
+  },
+})
+
+export type StandardScenario = ScenarioData<User, 'user'>
--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,6 +9,8 @@
 
 import { Router, Route, Set, Private } from '@redwoodjs/router'
 
+import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
+
 import { useAuth } from './auth'
 import ModularLayout from './layouts/ModularLayout/ModularLayout'
 import LoginPage from './pages/LoginPage/LoginPage'
@@ -19,6 +21,14 @@ const Routes = () => {
       <Route path="/login" page={LoginPage} name="login" />
       <Private unauthenticated="login">
         <Set wrap={ModularLayout}>
+          <Private unauthenticated="status" roles="admin">
+            <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
+              <Route path="/users/new" page={UserNewUserPage} name="newUser" />
+              <Route path="/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
+              <Route path="/users/{id:Int}" page={UserUserPage} name="user" />
+              <Route path="/users" page={UserUsersPage} name="users" />
+            </Set>
+          </Private>
           <Private unauthenticated="status" roles={['operator', 'admin']}>
             <Route path="/teleoperation" page={TeleoperationPage} name="teleoperation" />
           </Private>
